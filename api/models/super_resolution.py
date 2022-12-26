91eeca54142476eb9e0e0386ee598bc5836cc7f6
Andrés Pérez <alu0101313511@ull.edu.es>
2022-12-26 12:32:40 +0000

    Basic model caching

--- api/models/super_resolution.py
+++ api/models/super_resolution.py
@@ -4,9 +4,13 @@ import numpy as np
 from PIL import Image
 import tensorflow as tf
 import tensorflow_hub as hub
+from functools import lru_cache
+
 
 # TODO implement a generic model loader.
-model = hub.load("https://tfhub.dev/captain-pool/esrgan-tf2/1")
+@lru_cache
+def load_model():
+    return hub.load("https://tfhub.dev/captain-pool/esrgan-tf2/1")
 
 
 def enhance(image: Image.Image) -> Image.Image:
@@ -29,10 +33,15 @@ def enhance(image: Image.Image) -> Image.Image:
         low_input, 0, 0, input_size[0], input_size[1]
     )
     low_input = tf.cast(low_input, tf.float32)
+    # Since the model expects a group of images, thus we need 4 dimensions
+    # [batch_size, height, width, 3], so make a batch of size 1.
     low_input = tf.expand_dims(low_input, 0)
 
+    model = load_model()
     high_output = model(low_input)
+    # Extract single image from batch.
     high_output = tf.squeeze(high_output)
     high_output = tf.clip_by_value(high_output, 0, 255)
-    high_image = Image.fromarray(tf.cast(high_output, tf.uint8).numpy())
+    high_output = tf.cast(high_output, tf.uint8)
+    high_image = Image.fromarray(high_output.numpy())
     return high_image
